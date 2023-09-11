8c218cd996058ca6c306f9cca13a1f0ea4f4e796
brusnichkina <bru_snichkina@icloud.com>
2023-09-11 12:45:05 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+const array = Array.from(str);
+let result = array.join('');
+for (let i = 0; i < array.length; i++) {
+  for (let b = 0; b < bracketsConfig.length; b++) {
+    let n = bracketsConfig[b].join('');
+    result = result.replace(n, '');
+  };
+};
+if (result.length === 0) {
+  return true;
 }
+else {
+  return false;
+};
+};
